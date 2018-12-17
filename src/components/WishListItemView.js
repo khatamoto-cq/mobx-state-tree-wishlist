05a2d12de6e3f14757140e2d28ca149f80b82029
khatamoto-cq <k.hatamoto@caraquri.com>
2018-12-18 00:10:47 +0900

    I have done `Build Forms with React to Edit mobx-state-tree model`
    - How to call model actions from a component
    - Using clone to create a full, deep clone of any model instance
    - Using applySnapshot to update the state of a model instance given a
    snapshot

--- src/components/WishListItemView.js
+++ src/components/WishListItemView.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { observer } from "mobx-react";
+import WishListItemEdit from './WishListItemEdit'
+import { clone, getSnapshot, applySnapshot } from 'mobx-state-tree'
 
-const WishListItemView = ({item}) => (
-  <li className="item">
-    {item.image && <img src={item.image} />}
-    <h3>{item.name}</h3>
-    <span>{item.price}</span>
-  </li>
-)
+class WishListItemView extends Component {
+  constructor() {
+    super()
+    this.state = { isEditing: false }
+  }
+  render() {
+    const { item } = this.props
+    return (
+      this.state.isEditing ? (
+        this.renderEditable()
+      ) : (
+        <li className="item">
+          {item.image && <img src={item.image} />}
+          <h3>{item.name}</h3>
+          <span>{item.price}</span>
+          <span>
+          <button onClick={this.onToggleEdit}>✎</button>
+        </span>
+        </li>
+      )
+    )
+  }
 
-export default WishListItemView
+  renderEditable() {
+    return (
+      <li className="item">
+        <WishListItemEdit item={this.state.clone} />
+        <button onClick={this.onSaveEdit}>💢</button>
+        <button onClick={this.onCancelEdit}>✖</button>
+      </li>
+    )
+  }
+
+  onToggleEdit = () => {
+    this.setState({
+      isEditing: true,
+      clone: clone(this.props.item)
+    })
+  }
+
+  onCancelEdit = () => {
+    this.setState({ isEditing: false })
+  }
+
+  onSaveEdit = () => {
+    applySnapshot(this.props.item, getSnapshot(this.state.clone))
+    this.setState({
+      isEditing: false,
+      clone: null
+    })
+  }
+}
+
+export default observer(WishListItemView)
